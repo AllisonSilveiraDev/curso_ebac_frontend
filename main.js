5c549201026bab5edb1113b91aa00eeb3c3dfd0e
Allison Silveira <104576340+AllisonSilveiraDev@users.noreply.github.com>
2022-11-01 22:33:14 -0300

    Update main.js

--- main.js
+++ main.js
@@ -9,10 +9,14 @@ form.addEventListener("submit", function (e) {
   function validaForm() {
     if (valorA.value < valorB.value) {
       const msgDeSucesso = document.querySelector(".mensagem");
+      msgDeSucesso.classList.remove("error");
+      msgDeSucesso.classList.add("sucess");
       msgDeSucesso.style.display = "flex";
       msgDeSucesso.innerHTML = "Válido B é maior que A ";
     } else {
       const msgDeFalha = document.querySelector(".mensagem");
+      msgDeFalha.classList.remove("sucess");
+      msgDeFalha.classList.add("error");
       msgDeFalha.style.display = "flex";
       msgDeFalha.innerHTML = "Inválido B é menor que A ";
     }
